dc676fece3effda90de1ea7bcb3c851d528b5dab
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-29 09:53:52 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,33 +12,29 @@ public class Main {
     private static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws MyException{
-        ArrayList<Integer> arrayListChoose = new ArrayList<Integer>();
-        ArrayList<Integer> arrayListX1 = new ArrayList<Integer>();
-        ArrayList<Integer> arrayListX2 = new ArrayList<Integer>();
+        ArrayList<Integer> arrayListChoose = new ArrayList<>();
+        ArrayList<Integer> arrayListX1 = new ArrayList<>();
+        ArrayList<Integer> arrayListX2 = new ArrayList<>();
         ExecutorService executor = Executors.newFixedThreadPool(8);
 
         int choose, x1, x2, endCycle;
+        Scanner in = new Scanner(System.in);
         do {
             log.info("Choose:\n 1) f(x) = 1/ln(x)\n 2) f(x) = 7/(x^2+1)\n 3) f(x) = sin(x+1)\n Для выхода введите любой символ");
 
-            Scanner in = new Scanner(System.in);
+            do {
+                choose = Check.smartInput(in);
+            } while (!Check.isInt(choose, 1, 4));
 
-            try {
-                do {
-                    choose = in.nextInt();
-                } while (!Check.isInt(choose, 1, 4));
+            log.info("Input limits x1, x2 (-100000 .. 100000)");
+            do {
+                x1 = Check.smartInput(in);
+            } while (!Check.isInt(x1, -100000, 100000));
 
-                log.info("Input limits x1, x2 (-100000 .. 100000)");
-                do {
-                    x1 = in.nextInt();
-                } while (!Check.isInt(x1, -100000, 100000));
+            do {
+                x2 = Check.smartInput(in);
+            } while (!Check.isInt(x2, -100000, 100000, x1));
 
-                do {
-                    x2 = in.nextInt();
-                } while (!Check.isInt(x2, -100000, 100000, x1));
-            } catch (InputMismatchException e) {
-                throw new MyException("\nБыл введен символ. Выход.\n", ErrorCode.ErrorCheck);
-            }
 
             arrayListChoose.add(choose);
             arrayListX1.add(x1);
@@ -46,7 +42,7 @@ public class Main {
 
             log.info("Продолжить добавление? Да - 1 Нет - 2");
             do {
-                endCycle = in.nextInt();
+                endCycle = Check.smartInput(in);
             } while (!Check.isInt(endCycle, 1, 2));
         } while (endCycle != 2);
 
@@ -55,7 +51,7 @@ public class Main {
         }
         executor.shutdown();
         try {
-            executor.awaitTermination(arrayListChoose.size(), TimeUnit.SECONDS);
-        } catch (InterruptedException e) { throw new MyException("Время ожидание превышенно!", ErrorCode.ErrorCheck);}
+            executor.awaitTermination(arrayListChoose.size(), TimeUnit.MICROSECONDS);
+        } catch (InterruptedException e) { throw new MyException("Время ожидание превышенно!", ErrorCode.ErrorLimit);}
     }
 }
