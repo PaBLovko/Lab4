e20e027248496221a342c09481eef6797adb1004
PaBLovko <45830475+PaBLovko@users.noreply.github.com>
2019-05-29 09:52:34 +0300

    Update Check.java

--- src/main/java/Check.java
+++ src/main/java/Check.java
@@ -1,30 +1,31 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Check {
-    private static java.util.logging.Logger log = Logger.getLogger(Main.class.getName());
 
     public static boolean isInt(int integer, int limitLeft, int limitRight) throws MyException
     {
-        try {
-            if (integer >= limitLeft && integer <= limitRight)
-                return true;
-            else
-                throw new NumberFormatException();
-        } catch (NumberFormatException e) {
-            throw new MyException("\\nПроверьте правильность ввода.\\nМожете повторить попытку.",ErrorCode.ErrorCheck);
+        if (integer >= limitLeft && integer <= limitRight){
+            return true;
+        }
+        else {
+            throw new MyException("\\nПроверьте правильность ввода.\\nМожете повторить попытку.", ErrorCode.ErrorLimit);
         }
     }
 
     public static boolean isInt(int integer, int limitLeft, int limitRight, int x1) throws MyException
     {
-        try {
-            if(x1 < integer)
-                return Check.isInt(integer,limitLeft,limitRight);
-            else
-                throw new NumberFormatException();
-        } catch (NumberFormatException e) {
-            throw new MyException("\\nВторой предел должен быть больше первого.\\nМожете повторить попытку.",ErrorCode.ErrorCheck);
+        if(x1 < integer){
+            return Check.isInt(integer,limitLeft,limitRight);
         }
+        else{
+            throw new MyException("\\nВторой предел должен быть больше первого.\\nМожете повторить попытку.",ErrorCode.ErrorLimit);
+        }
+    }
+
+    public static int smartInput(Scanner input) throws MyException{
+        try {
+            return input.nextInt();
+        }catch (InputMismatchException e) {throw new MyException("\nБыл введен символ. Выход.\n", ErrorCode.ErrorLimit);}
     }
 }
